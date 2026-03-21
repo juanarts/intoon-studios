bdae1ba979fc30f68c0a0a821945dc6ee33cab15
juanarts <juanarts@github.com>
2026-03-21 17:19:55 +0100

    fix: validate UUID format in chargerParId to avoid 400 errors 🐛

--- js/models/Projet.js
+++ js/models/Projet.js
@@ -105,6 +105,10 @@ export default class Projet {
      * Recherche un projet spécifique par son identifiant
      */
     static async chargerParId(id) {
+        // Validation format UUID pour éviter erreur 400 PostgREST
+        const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+        if (!uuidRegex.test(id)) return null;
+
         try {
             const client = SupabaseService.getClient();
             const { data, error } = await client
